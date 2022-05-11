f0208427610e6255a506d1d9f56fe3796c41d53d
JullieoseuSijeo <105300366+JullieoseuSijeo@users.noreply.github.com>
2022-05-11 11:38:35 +0200

    Task Manager - Getter & Setter for Task -editTask() -CRUD-Comments

--- app/src/main/java/com/example/dailyhelper/controller/taskManager/TaskManagerController.java
+++ app/src/main/java/com/example/dailyhelper/controller/taskManager/TaskManagerController.java
@@ -5,19 +5,41 @@ import com.example.dailyhelper.taskManager.TaskCategory;
 
 public class TaskManagerController {
 
+    //CRUD-CREATE
     public Task createTask(Integer id, String name, TaskCategory category, String description, Integer duration, Integer priority){
         Task newTask = new Task(id, name, category, description, duration, priority);
+
+        //Save new Task Object to database
+
         return newTask;
     }
 
+    //CRUD-UPDATE: OldTask aus TaskList
     public Task editTask(Task oldTask, String name, TaskCategory category, String description, Integer duration, Integer priority){
         Task updatedTask = oldTask;
 
-        //Update the Task object and save it to database
+        if(name != null) {
+            updatedTask.setName(name);
+        }
+        if(category != null) {
+            updatedTask.setCategory(category);
+        }
+        if(description != null) {
+            updatedTask.setDescription(description);
+        }
+        if(duration != null) {
+            updatedTask.setDuration(duration);
+        }
+        if(priority != null) {
+            updatedTask.setPriority(priority);
+        }
+
+        //Save new Task Object to database
 
         return updatedTask;
     }
 
+    //CRUD-DELETE
     public Task deleteTask(Integer id) {
 
         //delete Task with matching ID from database
--- app/src/main/java/com/example/dailyhelper/taskManager/Task.java
+++ app/src/main/java/com/example/dailyhelper/taskManager/Task.java
@@ -21,5 +21,47 @@ public class Task {
         this.priority = priority;
     }
 
+    public Integer getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TaskCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(TaskCategory category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
 }
