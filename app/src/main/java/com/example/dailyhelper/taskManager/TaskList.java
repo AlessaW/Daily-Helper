78ef1d21636341406fcd3741751909f7c4f9148c
Janosch <jk255@hdm-stuttgart.de>
2022-05-11 11:28:39 +0200

    Deleted addTask and getNewTask

--- app/src/main/java/com/example/dailyhelper/taskManager/TaskList.java
+++ app/src/main/java/com/example/dailyhelper/taskManager/TaskList.java
@@ -9,11 +9,4 @@ public class TaskList {
     public TaskList(List<Task> tasks) {
         this.tasks = tasks;
     }
-
-    public void addTask(Task task) {
-        tasks.add(task);
-    }
-
-    public void getNewTaskId(){
-    }
 }
