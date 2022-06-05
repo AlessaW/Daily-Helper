2b85a3852266a8fc584aa20ec3a7506519c385e2
Jay.Byeun <jayay.byeun@gmail.com>
2022-06-05 21:48:45 +0200

    Added filters and button for Scheduler

---
+++ app/src/main/java/com/example/dailyhelper/model/scheduler/SchedulerActivity.java
@@ -0,0 +1,16 @@
+package com.example.dailyhelper.model.scheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dailyhelper.R;
+
+public class SchedulerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scheduler);
+    }
+}
