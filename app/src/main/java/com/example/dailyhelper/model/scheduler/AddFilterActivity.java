157e7c9f355617eb80e1412e198286d251a42a72
Jay.Byeun <jayay.byeun@gmail.com>
2022-06-16 14:48:31 +0200

    Added list

--- app/src/main/java/com/example/dailyhelper/model/scheduler/AddFilterActivity.java
+++ app/src/main/java/com/example/dailyhelper/model/scheduler/AddFilterActivity.java
@@ -2,18 +2,20 @@ package com.example.dailyhelper.model.scheduler;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.dailyhelper.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class AddFilterActivity extends AppCompatActivity {
 
     private static final String TAG = "AddFilterActivity";
     private Button resultButton;
+    private TextInputEditText duration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +23,34 @@ public class AddFilterActivity extends AppCompatActivity {
         setContentView(R.layout.fragment_add_duration);
         Log.d(TAG, "onCreate: Started");
 
-        resultButton = findViewById(R.id.resultButton);
+        resultButton  = findViewById(R.id.resultButton);
+        duration = findViewById(R.id.durationInput);
+
+        resultButton.setEnabled(false);
+
+        TextWatcher watcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                if (charSequence.toString().trim().length() == 0) {
+                    resultButton.setEnabled(false);
+                } else {
+                    resultButton.setEnabled(true);
+                    Log.d(TAG, "Input is provided");
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        };
+
+        duration.addTextChangedListener(watcher);
         resultButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d(TAG, "Show task list");
                 showResult();
             }
         });
@@ -34,5 +60,4 @@ public class AddFilterActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SchedulerActivity.class);
         startActivity(intent);
     }
-
 }
--- app/src/main/java/com/example/dailyhelper/model/scheduler/TaskListAdapter.java
+++ app/src/main/java/com/example/dailyhelper/model/scheduler/TaskListAdapter.java
@@ -6,12 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
-
 import com.example.dailyhelper.R;
-
-
 import java.util.ArrayList;
 
 public class TaskListAdapter extends ArrayAdapter<Task> {
