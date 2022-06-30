059da708e3623b53ea721b6ef52b4cd1cf76f5b6
Alexa56767 <49730170+Alexa56767@users.noreply.github.com>
2022-06-30 18:01:09 +0200

    scheduler Tests

--- app/src/test/java/com/example/dailyhelper/scheduler/SchedulerTests.java
+++ app/src/test/java/com/example/dailyhelper/scheduler/SchedulerTests.java
@@ -1,20 +1,61 @@
 package com.example.dailyhelper.scheduler;
 
+import android.content.Context;
 import android.util.Log;
 
-import com.example.dailyhelper.taskManager.Task;
-import com.example.dailyhelper.taskManager.TaskCategory;
-import com.example.dailyhelper.taskManager.TaskList;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.example.dailyhelper.model.database.AppDatabase;
+import com.example.dailyhelper.model.scheduler.IScheduler;
+import com.example.dailyhelper.model.scheduler.SimpleScheduler;
+import com.example.dailyhelper.model.taskmanager.Task;
+import com.example.dailyhelper.model.taskmanager.TaskCategory;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SchedulerTests {
 
-    Task task1 = new Task("task1", TaskCategory.GROCERY, "", 60, 3);
-    Task task2 = new Task("task2", TaskCategory.NATURE, "enjoying a walk in the park", 120, 4);
+    @Test
+        public void testData(){
 
-    TaskList list = new TaskList(new ArrayList<Task>());
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+                AppDatabase appDatabase = AppDatabase.getInMemoryDbInstance(appContext);
+
+
+                List<Task> list = new ArrayList<Task>();
+                list.add(new Task("Playing Winx Game", TaskCategory.GAMING, "Having Fun playing", 180, 1));
+                list.add(new Task("Se3 Project", TaskCategory.UNIVERSITY, "Creating Tests", 60, 2));
+                list.add(new Task("Another Task", TaskCategory.FRIENDS, "", 30, 3));
+                list.add(new Task("Task 4", TaskCategory.GROCERY, "", 30, 4));
+
+
+                IScheduler scheduler = new SimpleScheduler(appDatabase);
+
+
+                List<Task> resultList = scheduler.scheduleTasks(120, list);
+
+                String result = list.stream()
+                        .map(i -> i.getName())
+                        .collect(Collectors.joining(","));
+
+                Log.i("test", result);
+
+                Assert.assertNotNull(list.get(0));
+                Assert.assertEquals(
+                        new Task("Se3 Project", TaskCategory.UNIVERSITY, "Creating Tests", 60, 2),
+                        resultList.get(resultList.indexOf(list.get(1))));
+
+                Assert.assertTrue(resultList.contains(list.get(1)));
+                Assert.assertTrue(resultList.contains(list.get(2)));
+                Assert.assertTrue(resultList.contains(list.get(3)));
+                Assert.assertFalse(resultList.contains(list.get(0)));
+        }
 
 
 }
