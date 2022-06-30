151b2b36b8d536bf21f050a99a9d1acafb72e851
jk255 <jk255@hdm-stuttgart.de>
2022-06-30 15:12:23 +0200

    Fixed the priority again (now from 1-4, 1 is default)

--- app/src/main/java/com/example/dailyhelper/model/taskmanager/Task.java
+++ app/src/main/java/com/example/dailyhelper/model/taskmanager/Task.java
@@ -29,8 +29,10 @@ public class Task {
         this.description = description;
         this.duration = duration;
 
-        if (priority >=1 && priority <=5 ){
+        if (priority >=1 && priority <5 ){
             this.priority = priority;
+        } else {
+            this.priority = 1;
         }
     }
 
